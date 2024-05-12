f8e624e9ea847d5e78ed473587c444adb9818adb
Nguyen Tien Dat <datnt@Nguyens-iMac.local>
2024-05-12 22:00:33 +0700

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './modules/posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './core/database/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        PostsModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+        DatabaseModule,
+        UsersModule,
+        AuthModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
